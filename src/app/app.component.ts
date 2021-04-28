105558110a5ba850b4de5f79b7b51ef08cee6f60
Felipe Freitas <felipefreitas.2025@gmail.com>
2021-04-27 22:45:26 -0300

    mensagem

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { UsuarioServiceService } from './services/usuario/usuario-service.service';
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
@@ -11,5 +12,31 @@ export class AppComponent {
     { title: 'Registrar Carona', url: '/registrar-carona', icon: 'car' },
     { title: 'Confirmar Carona', url: '/confirmar-carona', icon: 'checkmark-circle' },
   ];
-  constructor() {}
+
+  usuario: { id, nome, email, cpf, dt_nascimento, sexo, senha } =
+    { id: null, nome: "", email: "", cpf: "", dt_nascimento: "", sexo: "", senha: "" };
+
+  constructor(public usuarioService: UsuarioServiceService) {}
+
+  ngOnInit() {
+    this.consultarUsuario(localStorage.getItem('usuario'));
+  }
+
+  consultarUsuario(cpf) {
+    if (cpf != null && cpf !== '') {
+      this.usuarioService.getByCpf(cpf).subscribe(dados => {
+        this.usuario = {
+          id: dados.id,
+          nome: dados.nome,
+          email: dados.email,
+          cpf: dados.cpf,
+          dt_nascimento: dados.dt_nascimento,
+          sexo: dados.sexo,
+          senha: dados.senha
+        };
+      });
+    } else {
+      this.usuario = { id: null, nome: "", email: "", cpf: "", dt_nascimento: "", sexo: "", senha: "" };
+    }
+  }
 }
